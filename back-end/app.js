6b670ceb87885364dce71de5396b019a4bf6f700
Lee <jihyeule@amazon.com>
2023-04-10 15:44:54 -0400

    Create Auth routes - user route, user controller, imported into and registered routers in the app.js

--- back-end/app.js
+++ back-end/app.js
@@ -1,27 +1,55 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const userRoutes = require('./routes/user');
+
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('Request received');
-    next();
+mongoose
+  .connect(
+    "mongodb+srv://happyfruitninja:OW6ECF67AzQhl8sr@cluster0.v9wwnqa.mongodb.net/test"
+  )
+  .then(() => {
+    console.log("Successfully connected to MongoDB Atlas.");
+  })
+  .catch((error) => {
+    console.log("Unable to connect to MongoDB Atlas.");
+    console.error(error);
+  });
+
+app.use("/api/sauces", (req, res, next) => {
+  const sauces = [
+    {
+      _id: "0000",
+      name: "sauce_0000",
+      manufacturer: "maker_0000",
+      description: "description_0000",
+      heat: 0,
+      likes: 0,
+      dislikes: 0,
+      imageUrl: "string",
+      mainPepper: "string",
+      usersLiked: "string[]",
+      usersDisliked: "string[]",
+      userId: "string",
+    },
+    {
+      _id: "1111",
+      name: "sauce_1111",
+      manufacturer: "maker_1111",
+      description: "description_1111",
+      heat: 1,
+      likes: 1,
+      dislikes: 1,
+      imageUrl: "string",
+      mainPepper: "string",
+      usersLiked: "string[]",
+      usersDisliked: "string[]",
+      userId: "string",
+    },
+  ];
+  res.status(200).json(sauces);
 });
 
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-});
 
-app.use((req, res, next) => {
-    res.json({message: 'Request was successful'});
-    next();
-});
-
-
-app.use((req, res, next) => {
-    console.log('Second console log message');
-    next();
-});
-
-
-
+app.use('/api/auth', userRoutes);
 module.exports = app;
