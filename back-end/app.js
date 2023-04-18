b6de9d1f5415b662d8f69d0be64feb0d4cd2a701
Lee <jihyeule@amazon.com>
2023-04-18 16:00:20 -0400

    created saucesRoutes and migrated relevant coding from app.js

--- back-end/app.js
+++ back-end/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/user");
 
-const Thing = require("./models/thing");
+const saucesRoutes = require('./routes/sauces'); //import saucesRoutes
 
 const app = express();
 app.use(express.json());
@@ -34,56 +34,6 @@ app.use((req, res, next) => {
 
 //app.use(bodyParser.json());
 
-app.post("/api/sauces", (req, res, next) => {
-  const thing = new Thing({
-    name: req.body.name,
-    manufacturer: req.body.manufacturer,
-    description: req.body.description,
-    heat: req.body.heat,
-    likes: req.body.likes,
-    dislikes: req.body.dislikes,
-    imageUrl: req.body.imageUrl,
-    mainPepper: req.body.mainPepper,
-    usersLiked: req.body.usersLiked,
-    usersDisliked: req.body.usersDisliked,
-    userId: req.body.userId,
-  });
-  thing
-    .save()
-    .then(() => {
-      res.status(201).json({
-        message: "Post saved successfully",
-      });
-    })
-    .catch((error) => {
-      res.status(400).json({
-        error: error,
-      });
-    });
-});
-
-//getting a specific thing
-app.get('/app/sauces/:id', (req, res, next) => {
-  Thing.findOne({
-    _id: req.params.id
-  }).then((thing) => {
-    res.status(200).json(thing);
-  }).catch((error) => {
-    res.status(404).json({error: error});
-  });
-})
-
-
-//getting a list of things
-app.use("/api/sauces", (req, res, next) => {
-  Thing.find()
-    .then((things) => {
-      res.status(200).json(things);
-    })
-    .catch((error) => {
-      res.status(400).json({error: error});
-    });
-});
-
+app.use("/api/sauces", saucesRoutes);
 app.use("/api/auth", userRoutes);
 module.exports = app;
